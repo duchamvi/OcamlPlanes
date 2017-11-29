85e056f8915e27fd46d1ec0300835b72ca449e12
Vincent Duchamp <vincent.duchamp@alumni.enac.fr>
2017-11-29 15:03:18 +0100

    29/11/17_test_detection2

--- conflicts.ml
+++ conflicts.ml
@@ -30,11 +30,14 @@ let assemble = fun known_clusters new_clusters ->
   Array.append known_clusters new_clusters
 
 let compare_tuple = fun elt tuple ->
+  (** compares the first element of a tuple with elt
+   used by common_beacon *)
     match tuple with
       (a,_) -> a = elt
  
 let common_beacon = fun p1 p2 ->
-  (** renvoie une structure contenant les balises communes *)
+  (** renvoie une structure contenant les balises communes à deux avions
+   used by two_planes_detection *)
   let balises_communes = [||] in
   let rec cbrec = fun balises1 balises2 communes ->
     match balises1 with
@@ -49,7 +52,8 @@ let common_beacon = fun p1 p2 ->
   cbrec p1.liste_balises p2.liste_balises balises_communes
 
 let pointproche = fun tabpoints t ->
-  (** Trouve le point le plus proche du temps t dans la liste *)
+  (** Trouve le point le plus proche du temps t dans la liste A AMELIORER
+   used by local_detection *)
   let pointfound = ref tabpoints.(0) in
   for i=0 to Array.length tabpoints -1
   do
@@ -61,15 +65,17 @@ let pointproche = fun tabpoints t ->
   !pointfound
 
 let selectpoint = fun t1 t2 point ->
-  (** renvoie le point dans un array s'il est entre t1 et t2 *)
+  (** renvoie le point dans un array s'il est entre t1 et t2
+   used by local_detection *)
   if t1 < point.temps && point.temps < t2
   then
     [| point |]
   else
     [||]
-  
+
 let local_detection = fun env p1 p2 beacon already_found ->
-  (** detects if p1 and p2 are in conflict near the beacon *)
+  (** detects if p1 and p2 are in conflict near the beacon
+   used by two_planes_detection *)
   if already_found
   then
     already_found
@@ -97,7 +103,8 @@ let local_detection = fun env p1 p2 beacon already_found ->
     !conflict_found
 
 let two_planes_detection = fun env p1 p2 conflict_clusters ->
-  (** Detects a conflict between p1 and p2 and adds it to a cluster *)
+  (** Detects a conflict between p1 and p2 and adds it to a cluster 
+   used by added_plane_detection *)
   if p1.fl = p2.fl
   then
     let communes = common_beacon p1 p2 in
@@ -121,11 +128,20 @@ let added_plane_detection = fun p planesinactivity env known_conflicts ->
 let () =
   let conflict_clusters = ref [||] in
   let env = {dseparation=5. *. 64.} in
+  
   (* avions *)
-  let a1 = {nom="a1";liste_balises=[("b",1)];tableau_point4D=[|{x=50;y=50;fl=1;temps=1;vx=1;vy=0};{x=1;y=1;fl=1;temps=1;vx=1;vy=0}|]; fl=1} in
+  let traj1 = [|{x=50;y=50;z=1;temps=1;vx=1;vy=0};{x=1;y=1;z=1;temps=1;vx=1;vy=0}|] in
+  let balises1 = [("b",1)] in
+  let a1 = {nom="a1";liste_balises=balises1;balisesmoins5=balises1;balisesplus5=balises1;tableau_point4D=traj1;trajmoins5=traj1; trajplus5=traj1; fl=1} in
   let planesinactivity = ref [|a1|] in
-  let a2 = {nom="a2";liste_balises=[("a",1);("b",1)];tableau_point4D=[|{x=100;y=100;fl=1;temps=1;vx=1;vy=0};{x=1;y=1;fl=1;temps=1;vx=1;vy=0}|]; fl=1} in
-  let a3 = {nom="a3";liste_balises=[("a",1)];tableau_point4D=[|{x=100;y=100;fl=1;temps=1;vx=1;vy=0};{x=10000;y=10000;fl=1;temps=1;vx=1;vy=0}|]; fl=1} in
+
+  let traj2 = [|{x=100;y=100;z=1;temps=1;vx=1;vy=0};{x=1;y=1;z=1;temps=1;vx=1;vy=0}|] in
+  let balises2 = [("a",1);("b",1)] in
+  let a2 = {nom="a2";liste_balises=balises2;balisesmoins5=balises2;balisesplus5=balises2;tableau_point4D=traj2;trajmoins5=traj2; trajplus5=traj2; fl=1} in
+  
+  let traj3 = [|{x=100;y=100;z=1;temps=1;vx=1;vy=0};{x=10000;y=10000;z=1;temps=1;vx=1;vy=0}|] in
+  let balises3 = [("a",1)] in
+  let a3 = {nom="a3";liste_balises=balises3;balisesmoins5=balises3;balisesplus5=balises3;tableau_point4D=traj3;trajmoins5=traj3; trajplus5=traj3; fl=1} in
 
   (* detection *)
   let ajout = fun a -> added_plane_detection a !planesinactivity env !conflict_clusters in
@@ -135,6 +151,3 @@ let () =
   conflict_clusters := ajout a3;
 
   Array.iter (fun x -> Printf.printf "(%s;%s)\n" x.(0).nom x.(1).nom) !conflict_clusters
-	     
- 
-			
